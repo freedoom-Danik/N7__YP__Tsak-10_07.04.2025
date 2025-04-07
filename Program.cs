2306128599ee573ecbb086346ec685823b27283a
Loli Admin <Loli Admin@DESKTOP-H4SR9KC>
2025-04-07 20:36:36 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace N7__YP__Tsak_10_07._04._2025
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("input string: ");
+            string input = Console.ReadLine();
+
+            string[] words = input.Split(new[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+            Array.Reverse(words);
+            string rusult = string.Join(" ", words);
+            Console.WriteLine(rusult);
+        }
+    }
+}
